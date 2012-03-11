42205862ce301f2aa7154e224b187bb5df46dedf
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-03-11 11:57:45 -0500

    disable 7x30 and 8660 targets (until build fail is resolved)

--- Android.mk
+++ Android.mk
@@ -8,22 +8,22 @@ ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
     include $(OMX_VIDEO_PATH)/qdsp6/venc/Android.mk
 endif
 
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
-    include $(OMX_VIDEO_PATH)/vidc/vdec/Android.mk
-    include $(OMX_VIDEO_PATH)/vidc/venc/Android.mk
-    include $(OMX_VIDEO_PATH)/DivxDrmDecrypt/Android.mk
-endif
+#ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+#    include $(OMX_VIDEO_PATH)/vidc/vdec/Android.mk
+#    include $(OMX_VIDEO_PATH)/vidc/venc/Android.mk
+#    include $(OMX_VIDEO_PATH)/DivxDrmDecrypt/Android.mk
+#endif
 
-ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
-    include $(OMX_VIDEO_PATH)/vidc/vdec/Android.mk
-    include $(OMX_VIDEO_PATH)/vidc/venc/Android.mk
-    include $(OMX_VIDEO_PATH)/DivxDrmDecrypt/Android.mk
-endif
+#ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
+#    include $(OMX_VIDEO_PATH)/vidc/vdec/Android.mk
+#    include $(OMX_VIDEO_PATH)/vidc/venc/Android.mk
+#    include $(OMX_VIDEO_PATH)/DivxDrmDecrypt/Android.mk
+#endif
 
-ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
-    include $(OMX_VIDEO_PATH)/vidc/vdec/Android.mk
-    include $(OMX_VIDEO_PATH)/vidc/venc/Android.mk
-    include $(OMX_VIDEO_PATH)/DivxDrmDecrypt/Android.mk
-endif
+#ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+#    include $(OMX_VIDEO_PATH)/vidc/vdec/Android.mk
+#    include $(OMX_VIDEO_PATH)/vidc/venc/Android.mk
+#    include $(OMX_VIDEO_PATH)/DivxDrmDecrypt/Android.mk
+#endif
 
 endif #BOARD_USES_QCOM_HARDWARE
